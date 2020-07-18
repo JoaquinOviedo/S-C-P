b7d047278484e77d8c396bd2f6217bc553fde3b5
Joaquin Oviedo <joaco_oviedo2@hotmail.com>
2020-07-18 02:32:29 -0300

    Steamgamescardsdropprofit v1.1
    Mejorada la sintaxis.
    Ahora se puede ingresar directamente el link de la store de steam del juego, obtiene el precio que tiene el juego en pesos ARS, y calcula el profit que se obtiene vendiendo los cromos.
    Ahora toma hasta 10 cromos para calcular el promedio

--- steam scraping.py
+++ steam scraping.py
@@ -1,130 +1,92 @@
 from bs4 import BeautifulSoup
 import requests
 import json
-print("ingresa el appid del juego: ")
-appid = input()
-appid2= appid.replace("\0","")
-html_doc = requests.get("https://steamcommunity.com/market/search?q=&category_753_Game%5B%5D=tag_app_"+appid2+"&category_753_cardborder%5B%5D=tag_cardborder_0&category_753_item_class%5B%5D=tag_item_class_2&appid=753")
 
-soup = BeautifulSoup(html_doc.content, 'html.parser')
-numerosdecartas = soup.find(id="searchResults_total")
+#pide el appid y usa la libreria beatifulsoup y requests para leer la pagina
+print("Ingrese la url del juego ")
+urljuego = input()
+urljuegodata= urljuego.split("/")
+appid= urljuegodata[4]
+
+store_doc = requests.get("https://store.steampowered.com/api/appdetails?appids="+appid+"&cc=ars&filters=price_overview")
+storesoup= BeautifulSoup(store_doc.content,"html.parser")
+strstoresoup= str(storesoup)
+storejsonprecio= json.loads(strstoresoup)                                        #lee un objeto en json de un documento , y transforma el json en un string
+strprecio= storejsonprecio[appid]["data"]["price_overview"]["final_formatted"]   # lee lo que pertenece a final_formatted en el json
+
+print("El juego "+urljuegodata[5]+ " cuesta: "+ strprecio)
+
+precioreplace= strprecio.replace("ARS$","")
+precioreplace2= precioreplace.replace(",",".")
+preciodeljuego= float(precioreplace2)
+
+market_doc = requests.get("https://steamcommunity.com/market/search?q=&category_753_Game%5B%5D=tag_app_"+appid+"&category_753_cardborder%5B%5D=tag_cardborder_0&category_753_item_class%5B%5D=tag_item_class_2&appid=753")
+
+marketsoup = BeautifulSoup(market_doc.content, "html.parser")
+
+#lee el total de numeros de carta
+numerosdecartas = marketsoup.find(id="searchResults_total")
 Ncartas=numerosdecartas.get_text()
-Cartas=float(Ncartas)
+Cartas=int(Ncartas)
+cartas=Cartas
 if Cartas%2==0 :
       Dropcartas=Cartas/2
 if Cartas%2==1:
       Dropcartas=(Cartas/2)+0.5
+
+
+if Cartas>10:
+      cartas=10
       
-cardprice0 = soup.find(id="resultlink_0")
-cardprice1 = soup.find(id="resultlink_1")
-cardprice2 = soup.find(id="resultlink_2")
-cardprice3 = soup.find(id="resultlink_3")
-cardprice4 = soup.find(id="resultlink_4")
+Numerocartas= marketsoup.find(id="searchResultsRows")
+gettextnumerocartas = Numerocartas.get_text()
 
-price0tostring = cardprice0.get_text()
-price0parseusd = price0tostring.split("\n")
+Numerocartasparsed= gettextnumerocartas.split("\n")
 
-price1tostring = cardprice1.get_text()
-price1parseusd = price1tostring.split("\n")
 
-price2tostring = cardprice2.get_text()
-price2parseusd = price2tostring.split("\n")
+Director=""
+urlprecio=""
+jsonget=""
+strprecio=""
+sumadeprecios=0
+volumenes=0
+for directores in range(0,cartas):
+      Director= Numerocartasparsed[29+27*directores]
 
-price3tostring = cardprice3.get_text()
-price3parseusd = price3tostring.split("\n")
+      urlprecio = "https://steamcommunity.com/market/priceoverview/?currency=34&appid=753&market_hash_name="+appid+"-"+Director
+      jsonget = requests.get(urlprecio)
+      soupprecio =BeautifulSoup(jsonget.content, 'html.parser')
+      soupstrprecio= str(soupprecio)
+      jsonprecio= json.loads(soupstrprecio)                                   #lo convierte en json 
+      strprecio= jsonprecio["lowest_price"].replace("ARS$ ","")
+      str2precio= strprecio.replace(",",".")
+      precio= float(str2precio)
+      strvolumen=jsonprecio["volume"]
+      str2volumen= strvolumen.replace(",","")
+      volumen= float(str2volumen)
+      
+      sumadeprecios+= precio
+      volumenes+= volumen
 
-price4tostring = cardprice4.get_text()
-price4parseusd = price4tostring.split("\n")
 
-urlprecio0 = "https://steamcommunity.com/market/priceoverview/?currency=34&appid=753&market_hash_name="+appid2+"-"+price0parseusd[20]
-
-urlprecio1 = "https://steamcommunity.com/market/priceoverview/?currency=34&appid=753&market_hash_name="+appid2+"-"+price1parseusd[20]
-
-urlprecio2 = "https://steamcommunity.com/market/priceoverview/?currency=34&appid=753&market_hash_name="+appid2+"-"+price2parseusd[20]
-
-urlprecio3 = "https://steamcommunity.com/market/priceoverview/?currency=34&appid=753&market_hash_name="+appid2+"-"+price3parseusd[20]
-
-urlprecio4 = "https://steamcommunity.com/market/priceoverview/?currency=34&appid=753&market_hash_name="+appid2+"-"+price4parseusd[20]
-
-json0 = requests.get(urlprecio0)
-soupprecio0 =BeautifulSoup(json0.content, 'html.parser')
-strprecio0= str(soupprecio0)                                                                                       #pasa de soup a string.
-jsonprecio0= json.loads(strprecio0)
-
-json1 = requests.get(urlprecio1)
-soupprecio1 =BeautifulSoup(json1.content, 'html.parser')
-strprecio1= str(soupprecio1)                                                                                       
-jsonprecio1= json.loads(strprecio1)
-
-json2 = requests.get(urlprecio2)
-soupprecio2 =BeautifulSoup(json2.content, 'html.parser')
-strprecio2= str(soupprecio2)                                                                                       
-jsonprecio2= json.loads(strprecio2)
-
-json3 = requests.get(urlprecio3)
-soupprecio3 =BeautifulSoup(json3.content, 'html.parser')
-strprecio3= str(soupprecio3)                                                                                       
-jsonprecio3= json.loads(strprecio3)
-
-json4 = requests.get(urlprecio4)
-soupprecio4 =BeautifulSoup(json4.content, 'html.parser')
-strprecio4= str(soupprecio4)                                                                                       
-jsonprecio4= json.loads(strprecio4)
-
-strprecio0= jsonprecio0["lowest_price"].replace("ARS$ ","")
-strprecio1= jsonprecio1["lowest_price"].replace("ARS$ ","")
-strprecio2= jsonprecio2["lowest_price"].replace("ARS$ ","")
-strprecio3= jsonprecio3["lowest_price"].replace("ARS$ ","")
-strprecio4= jsonprecio4["lowest_price"].replace("ARS$ ","")
-
-str2precio0= strprecio0.replace(",",".")
-str2precio1= strprecio1.replace(",",".")
-str2precio2= strprecio2.replace(",",".")
-str2precio3= strprecio3.replace(",",".")
-str2precio4= strprecio4.replace(",",".")
-
-precio0= float(str2precio0)
-precio1= float(str2precio1)
-precio2= float(str2precio2)
-precio3= float(str2precio3)
-precio4= float(str2precio4)
-
-promedioprecios= (precio0+precio1+precio2+precio3+precio4)*Dropcartas*0.87/5 
+promedioprecios=sumadeprecios*0.87*Dropcartas/cartas
 promedioprecios= round(promedioprecios,2)
-
-strvolumen0=jsonprecio0["volume"]
-strvolumen1=jsonprecio1["volume"]
-strvolumen2=jsonprecio2["volume"]
-strvolumen3=jsonprecio3["volume"]
-strvolumen4=jsonprecio4["volume"]
-
-str2volumen0= strvolumen0.replace(",","")
-str2volumen1= strvolumen1.replace(",","")
-str2volumen2= strvolumen2.replace(",","")
-str2volumen3= strvolumen3.replace(",","")
-str2volumen4= strvolumen4.replace(",","")
-
-volumen0= float(str2volumen0)
-volumen1= float(str2volumen1)
-volumen2= float(str2volumen2)
-volumen3= float(str2volumen3)
-volumen4= float(str2volumen4)
-
-
-volumenes= (volumen0+volumen1+volumen2+volumen3+volumen4)
-
 strpromedioprecios= str(promedioprecios)
 
 
-if volumenes>=200 :
-    strvolumenes="se venden muchos por dia"
-if volumenes<200 and volumenes>=100  :
-     strvolumenes="se venden bastantes por dia"
+if volumenes>=250 :
+     strvolumenes="Se venden mas de 250 cromos por dia"
+if volumenes<250 and volumenes>=100 :
+      strvolumenes="Se venden entre 100 y 250 cromos por dia"
 
 if volumenes<100 and volumenes>=20 :
-      strvolumenes="se venden pocos por dia"
+       strvolumenes="Se venden entro 20 y 100 por dia"
 if  volumenes<20 :
-      strvolumenes="casi no se venden por dia"
+       strvolumenes="Se venden menos de 20 cromos por dia por dia"
 
-print("vendiendo los cromos sacas : "+ strpromedioprecios)
+profit=promedioprecios-preciodeljuego
+profit= round(profit,2)
+strprofit= str(profit)
+print("Vendiendo los cromos se obtiene : "+ strpromedioprecios)
+print("Profit: "+strprofit)
 print(strvolumenes)
